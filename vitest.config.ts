fe989ca67994739882365342619069c04204d44d
Gabriel <gabrielvitor.frasao@gmail.com>
2022-08-08 18:17:03 -0300

    initial setup

---
+++ vitest.config.ts
@@ -0,0 +1,15 @@
+import react from "@vitejs/plugin-react";
+  import tsconfigPaths from "vite-tsconfig-paths";
+  import { defineConfig } from "vitest/config";
+
+  export default defineConfig({
+    plugin: [tsconfigPaths(), react()],
+    test: {
+      environment: "jsdom",
+      setupFiles: ["./core/tests/setup.ts"],
+      coverage: {
+        enabled: true,
+        include: ["./components/**/*.tsx"],
+      },
+    },
+  });
