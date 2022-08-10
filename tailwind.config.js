bcb4ba456abaeb5d863ceebfbd60141baed320b5
Gabriel <gabrielvitor.frasao@gmail.com>
2022-08-09 22:45:29 -0300

    chore: add custom font family and themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,47 @@
 /** @type {import(tailwindcss).Config} */
-  module.exports = {
-    content: [
-      "./pages/**/*.{js,ts,jsx,tsx}",
-      "./components/**/*.{js,ts,jsx,tsx}",
-      ],
-    theme: {
-      extend: {
-        container: {
-          center: true,
-          padding: "1rem",
-        },
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: "1rem",
+      },
+      fontFamily: {
+        sans: ["Space Grotesk", "sans-serif"],
       },
     },
-    plugins: [
-      // require("@tailwindcss/forms"),
-      // require("tailwind-scrollbar"),
-      require("@tailwindcss/typography"),
-      require("daisyui"),
+  },
+  plugins: [
+    // require("@tailwindcss/forms"),
+    // require("tailwind-scrollbar"),
+    require("daisyui"),
+  ],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          primary: "#22223B",
+          secondary: "#4A4E69",
+          accent: "#9A8C98",
+          neutral: "#000000",
+          "base-100": "#FFFFFF",
+          "base-content": "#22223B",
+        },
+      },
+      {
+        dark: {
+          primary: "#22223B",
+          secondary: "#4A4E69",
+          accent: "#9A8C98",
+          neutral: "#000000",
+          "base-100": "#000000",
+          "base-content": "#FFFFFF",
+        },
+      },
     ],
-    daisyui: {
-      themes: ["dark", "light"],
-    }
-  }
+  },
+};
