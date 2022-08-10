8b6803dde2c7899758df41120a770762e52a7799
Gabriel <gabrielvitor.frasao@gmail.com>
2022-08-09 22:46:40 -0300

    create Navbar

---
+++ components/modules/Navbar/Navbar.tsx
@@ -0,0 +1,64 @@
+import { useTheme } from "@core/hooks";
+import Link from "next/link";
+import { useState } from "react";
+
+export const Navbar = () => {
+  const { theme } = useTheme();
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  function handleToggleMenu() {
+    setIsMobileMenuOpen((prev) => !prev);
+  }
+
+  return (
+    <nav className="py-3 flex">
+      <Link href="/">
+        <a className="mr-auto">
+          <img
+            src={
+              theme === "light"
+                ? "/images/logo-minimal-black.svg"
+                : "/images/logo-minimal-white.svg"
+            }
+            alt="Ence logo"
+          />
+        </a>
+      </Link>
+
+      {/* menu bar on desktop screens */}
+      <ul className="mr-4 hidden sm:flex gap-2">
+        <li>
+          <Link href="/about">
+            <a>Portfólio</a>
+          </Link>
+        </li>
+
+        <li>
+          <Link href="/contact">
+            <a>Sobre</a>
+          </Link>
+        </li>
+      </ul>
+
+      {/* button to toggle language (only on desktop screens) */}
+      <button type="button" className="hidden sm:flex">
+        EN
+      </button>
+
+      {/* menu bar on mobile screens */}
+      <button
+        type="button"
+        onClick={handleToggleMenu}
+        className="relative sm:hidden w-16 h-[22px]"
+      >
+        <div className="absolute top-0 w-full h-[6px] bg-base-content" />
+
+        <div
+          className={`absolute bottom-0 ${
+            isMobileMenuOpen ? "left-0" : "left-16 -translate-x-full"
+          } w-[14px] h-auto aspect-square rounded-full bg-base-content transition-all duration-300`}
+        />
+      </button>
+    </nav>
+  );
+};
