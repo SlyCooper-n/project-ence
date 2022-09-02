34934888ca0a3dcdf04810e82c3b4e3f0ee1794c
Gabriel <gabrielvitor.frasao@gmail.com>
2022-09-02 20:35:44 -0300

    fix(button): fix margin on hover

--- components/widgets/Button/Button.tsx
+++ components/widgets/Button/Button.tsx
@@ -1,20 +1,22 @@
 import { useTheme } from "@core/hooks";
 import { ButtonProps } from "@core/types";
 
-export const Button = (props: ButtonProps) => {
+export const Button = ({ className, children, ...rest }: ButtonProps) => {
   const { theme } = useTheme();
 
   return (
     <button
-      type="button"
+      {...rest}
       className={`group h-7 lg:h-10 flex flex-row justify-center items-center gap-0 hover:gap-5 transition-all ${
         theme === "dark" ? "bg-secondary" : "bg-primary"
-      } text-white ${props.className}`}
+      } text-white ${className}`}
     >
-      <div className="text-xs lg:text-sm">{props.children}</div>
+      <div className="text-xs lg:text-sm -mr-5 group-hover:mr-0">
+        {children}
+      </div>
 
       <div
-        className={`w-5 h-full flex justify-center items-center opacity-0 group-hover:opacity-100 transition-opacity`}
+        className={`h-full flex justify-center items-center opacity-0 group-hover:opacity-100 scale-0 group-hover:scale-100 transition-all`}
       >
         <img src="/icons/arrow.svg" alt="Right arrow" className="w-5" />
       </div>
