32bba96527d2d7bc5019fc6a6ff22f1ad6824102
Gabriel <gabrielvitor.frasao@gmail.com>
2022-09-02 16:48:42 -0300

    refactor(footer): change content depending on the language

--- components/modules/Footer/Footer.tsx
+++ components/modules/Footer/Footer.tsx
@@ -1,3 +1,13 @@
+import { useRouter } from "next/router";
+
 export const Footer = () => {
-  return <div className="flex pt-8 pb-5">Ence &copy; 2020-2022</div>;
+  const isInEnglish = useRouter().pathname.includes("/en");
+
+  return (
+    <div className="flex pt-8 pb-5">
+      Ence{" "}
+      {isInEnglish ? "all rights reserved" : "todos os direitos reservados"}{" "}
+      2020-2022
+    </div>
+  );
 };
