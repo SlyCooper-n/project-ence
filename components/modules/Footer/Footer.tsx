f9c0bfcaec81972d3be7100a099cb9da6ff9bb08
Gabriel <gabrielvitor.frasao@gmail.com>
2022-09-02 17:26:24 -0300

    feat(footer): add "developed by" content
    add content and link to my portfolio

--- components/modules/Footer/Footer.tsx
+++ components/modules/Footer/Footer.tsx
@@ -5,9 +5,17 @@ export const Footer = () => {
 
   return (
     <div className="flex pt-8 pb-5 text-xs sm:text-base">
-      Ence -{" "}
-      {isInEnglish ? "all rights reserved" : "todos os direitos reservados"}{" "}
-      2020-2022
+      {isInEnglish
+        ? "Ence - all rights reserved 2020-2022 | Developed by "
+        : "Ence - todos os direitos reservados 2020-2022 | Desenvolvido por "}
+      <a
+        href="https://github.com/SlyCooper-n"
+        target="_blank"
+        rel="noreferrer"
+        className="ml-1"
+      >
+        Gabriel VS Frasão
+      </a>
     </div>
   );
 };
