8554002323ca8f051f4bde8ee6ad0b62b7e8721a
Gabriel <gabrielvitor.frasao@gmail.com>
2022-09-02 18:37:40 -0300

    feat(themecontext): save selected theme as cookie

--- core/contexts/ThemeContext.tsx
+++ core/contexts/ThemeContext.tsx
@@ -1,13 +1,21 @@
 import { ThemeContextValues, ThemeProviderProps } from "@core/types";
+import { parseCookies, setCookie } from "nookies";
 import { createContext, useState } from "react";
 
 export const ThemeContext = createContext({} as ThemeContextValues);
 
 export const ThemeProvider = ({ children }: ThemeProviderProps) => {
-  const [theme, setTheme] = useState<"light" | "dark">("light");
+  const [theme, setTheme] = useState<"light" | "dark">(
+    (parseCookies()["ence_studio_theme"] as "light" | "dark") ?? "light"
+  );
 
   function toggleTheme() {
     setTheme(theme === "light" ? "dark" : "light");
+
+    setCookie(null, "ence_studio_theme", theme === "light" ? "dark" : "light", {
+      maxAge: 86400 * 7,
+      path: "/",
+    });
   }
 
   return (
