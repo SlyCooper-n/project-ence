6140c7af53368941565a8f76967f75c466990723
Gabriel <gabrielvitor.frasao@gmail.com>
2022-09-02 20:36:30 -0300

    feat(home): add animation to about-section

--- components/pages/Home/Home.tsx
+++ components/pages/Home/Home.tsx
@@ -5,12 +5,22 @@ import { useTheme } from "@core/hooks";
 import { HomeProps } from "@core/types";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import {
+  BehanceLogo,
+  GithubLogo,
+  InstagramLogo,
+  LinkedinLogo,
+} from "phosphor-react";
+import { useState } from "react";
 
 export const Home = ({ cmsData }: HomeProps) => {
   const { seo, heading, highlightedCases, about } = cmsData;
 
   const { theme } = useTheme();
   const isInEnglish = useRouter().pathname.includes("/en");
+  const [step, setStep] = useState<0 | 1 | 2>(0);
+
+  console.log(step);
 
   return (
     <PageContainer headTitle={seo.title} description={seo.description}>
@@ -81,10 +91,14 @@ export const Home = ({ cmsData }: HomeProps) => {
       {/* People */}
       <section
         id="about"
-        className="w-4/5 sm:w-auto mx-auto mb-16 sm:mb-32 text-center"
+        className="relative w-4/5 sm:w-auto mx-auto mb-16 sm:mb-32 text-center"
       >
         <div className="flex flex-col xl:flex-row xl:items-center">
-          <div className="xl:flex-1 xl:mr-4">
+          <div
+            className={`xl:flex-1 xl:mr-4 transition-all ${
+              step > 0 && "lg:opacity-0"
+            } transition-all`}
+          >
             <h2 className="mb-2 sm:mb-4 sm:text-5xl xl:text-start">
               {about.title}
             </h2>
@@ -93,28 +107,82 @@ export const Home = ({ cmsData }: HomeProps) => {
               {about.subtitle}
             </p>
 
-            <Button type="button" className="w-[200px] hidden xl:flex">
+            <Button
+              type="button"
+              onClick={() =>
+                setStep((prev) => (prev++ > 2 ? 0 : (prev++ as 0 | 1 | 2)))
+              }
+              className="w-[200px] hidden xl:flex"
+            >
               {isInEnglish ? "See more" : "Ver mais"}
             </Button>
           </div>
 
-          <div className="xl:flex-[2] relative mb-8 flex justify-center xl:justify-end">
+          <div className="xl:flex-[2] mb-8 flex justify-center xl:justify-end">
             {about.people.map((person, i) => (
-              <img
-                key={person.id}
-                src={person.profilePicture.url}
-                alt={`${person.personName} profile picture`}
-                className={`max-w-[200px] sm:max-w-[300px] md:max-w-[450px] rounded-full ${
-                  i === 0
-                    ? "translate-x-1/4 xl:translate-x-[60%] z-10"
-                    : "-translate-x-1/4 xl:translate-x-0 z-0"
-                }`}
-              />
+              <>
+                <img
+                  key={person.id}
+                  src={person.profilePicture.url}
+                  alt={`${person.personName} profile picture`}
+                  className={`max-w-[200px] sm:max-w-[300px] md:max-w-[450px] rounded-full ${
+                    i === 0
+                      ? `translate-x-1/4 xl:translate-x-[60%] z-10 ${
+                          step === 1 && "absolute -left-1/3 translate-x-0"
+                        } ${step === 2 && "opacity-0"}`
+                      : `-translate-x-1/4 xl:translate-x-0 z-0 ${
+                          step === 1 && "opacity-0"
+                        } ${
+                          step === 2 &&
+                          "absolute opacity-100 -left-1/3 lg:-left-16 translate-x-0"
+                        }`
+                  } transition-all`}
+                />
+
+                <div
+                  className={`${
+                    step === 0 && "opacity-0"
+                  } absolute top-1/3 left-1/2 ${
+                    i === 0
+                      ? `${step === 1 && ""} ${step === 2 && "opacity-0"}`
+                      : `${step === 1 && "opacity-0"}`
+                  } transition-all`}
+                >
+                  <p className="text-[8px] sm:text-base">{person.bio.text}</p>
+
+                  <ul className="mt-4 flex justify-center items-center gap-2">
+                    {person.socialMedia.map((media) => (
+                      <li key={media.socialMedia}>
+                        <a href={media.url} target="_blank" rel="noreferrer">
+                          {media.socialMedia === "behance" && (
+                            <BehanceLogo size={30} />
+                          )}
+                          {media.socialMedia === "instagram" && (
+                            <InstagramLogo size={30} />
+                          )}
+                          {media.socialMedia === "github" && (
+                            <GithubLogo size={30} />
+                          )}
+                          {media.socialMedia === "linkedin" && (
+                            <LinkedinLogo size={30} />
+                          )}
+                        </a>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </>
             ))}
           </div>
         </div>
 
-        <Button type="button" className="w-[148px] mx-auto xl:hidden">
+        <Button
+          type="button"
+          onClick={() =>
+            setStep((prev) => (prev++ > 1 ? 0 : (prev++ as 1 | 2)))
+          }
+          className={`w-[148px] mx-auto ${step > 0 ? "" : "xl:hidden"}`}
+        >
           {isInEnglish ? "See more" : "Ver mais"}
         </Button>
       </section>
