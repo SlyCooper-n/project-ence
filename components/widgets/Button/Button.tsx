27968090c2b51c51f6f8ec56dff06776d5158306
Gabriel <gabrielvitor.frasao@gmail.com>
2022-09-02 16:50:41 -0300

    refactor(button): align content on center

--- components/widgets/Button/Button.tsx
+++ components/widgets/Button/Button.tsx
@@ -7,14 +7,14 @@ export const Button = (props: ButtonProps) => {
   return (
     <button
       type="button"
-      className={`h-7 lg:h-10 flex flex-row items-center ${
+      className={`h-7 lg:h-10 flex flex-row justify-center items-center gap-5 ${
         theme === "dark" ? "bg-secondary" : "bg-primary"
       } text-white ${props.className}`}
     >
-      <div className="flex-1 text-xs lg:text-sm">{props.children}</div>
+      <div className="text-xs lg:text-sm">{props.children}</div>
 
-      <div className={`w-[23%] h-full flex justify-center items-center`}>
-        <img src="/icons/arrow.svg" alt="Right arrow" className="w-2/3" />
+      <div className={`h-full flex justify-center items-center`}>
+        <img src="/icons/arrow.svg" alt="Right arrow" className="w-5" />
       </div>
     </button>
   );
