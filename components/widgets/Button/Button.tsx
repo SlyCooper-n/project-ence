d7d291a3d32becf4362fc5681da67b497980026a
Gabriel <gabrielvitor.frasao@gmail.com>
2022-09-02 18:00:07 -0300

    feat(button): add animation on hover

--- components/widgets/Button/Button.tsx
+++ components/widgets/Button/Button.tsx
@@ -7,11 +7,17 @@ export const Button = (props: ButtonProps) => {
   return (
     <button
       type="button"
-      className={`h-7 lg:h-10 flex flex-row justify-center items-center ${
+      className={`group h-7 lg:h-10 flex flex-row justify-center items-center gap-0 hover:gap-5 transition-all ${
         theme === "dark" ? "bg-secondary" : "bg-primary"
       } text-white ${props.className}`}
     >
       <div className="text-xs lg:text-sm">{props.children}</div>
+
+      <div
+        className={`w-5 h-full flex justify-center items-center opacity-0 group-hover:opacity-100 transition-opacity`}
+      >
+        <img src="/icons/arrow.svg" alt="Right arrow" className="w-5" />
+      </div>
     </button>
   );
 };
