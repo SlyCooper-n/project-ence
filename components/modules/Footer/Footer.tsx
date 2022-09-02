9384d5e4cdf4573308b5bbfac85326e0ac03f0ea
Gabriel <gabrielvitor.frasao@gmail.com>
2022-09-02 17:01:45 -0300

    refactor(footer): add "-" between text

--- components/modules/Footer/Footer.tsx
+++ components/modules/Footer/Footer.tsx
@@ -4,8 +4,8 @@ export const Footer = () => {
   const isInEnglish = useRouter().pathname.includes("/en");
 
   return (
-    <div className="flex pt-8 pb-5">
-      Ence{" "}
+    <div className="flex pt-8 pb-5 text-xs sm:text-base">
+      Ence -{" "}
       {isInEnglish ? "all rights reserved" : "todos os direitos reservados"}{" "}
       2020-2022
     </div>
