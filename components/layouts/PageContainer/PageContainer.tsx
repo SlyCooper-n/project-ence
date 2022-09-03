0b6e43bb3ec19b889d65899bfe57ee8220dc14a6
Gabriel <gabrielvitor.frasao@gmail.com>
2022-09-02 21:28:51 -0300

    fix(favicons): fix favicons relative path
    add '/' before each favicon name in order to fetch from the root

--- components/layouts/PageContainer/PageContainer.tsx
+++ components/layouts/PageContainer/PageContainer.tsx
@@ -18,14 +18,14 @@ export const PageContainer = (props: PageContainerProps) => {
     ) as HTMLLinkElement;
 
     if (matchMedia("(prefers-color-scheme: dark)").matches) {
-      faviconLink.href = "favicon-white.png";
-      touchIconLink.href = "touch-icon-white.png";
+      faviconLink.href = "/favicon-white.png";
+      touchIconLink.href = "/touch-icon-white.png";
 
       return;
     }
 
-    faviconLink.href = "favicon.png";
-    touchIconLink.href = "touch-icon.png";
+    faviconLink.href = "/favicon.png";
+    touchIconLink.href = "/touch-icon.png";
   }, []);
 
   return (
