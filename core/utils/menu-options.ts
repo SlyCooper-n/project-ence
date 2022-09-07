fdda0df7f2a0a60bc6bb78777eea7073548b2fef
Gabriel <gabrielvitor.frasao@gmail.com>
2022-09-07 17:32:12 -0300

    feat(menu-options): create menu options object to easily manage the navbuttons

---
+++ core/utils/menu-options.ts
@@ -0,0 +1,23 @@
+export const menuOptions = [
+  {
+    path: "/projects",
+    en_path: "/en/projects",
+    name: "Portfólio",
+    en_name: "Portfolio",
+    mobile: true,
+  },
+  {
+    path: "/#about",
+    en_path: "/en#about",
+    name: "Sobre",
+    en_name: "About",
+    mobile: true,
+  },
+  {
+    path: "/budget",
+    en_path: "/en/budget",
+    name: "Orçamento",
+    en_name: "Budget",
+    mobile: true,
+  },
+];
