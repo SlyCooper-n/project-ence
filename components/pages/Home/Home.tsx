d01c0618f38b20079f9f596ac2017226e213c5dd
Gabriel <gabrielvitor.frasao@gmail.com>
2022-09-16 18:46:19 -0300

    refactor(homepage): update about section layout

--- components/pages/Home/Home.tsx
+++ components/pages/Home/Home.tsx
@@ -2,23 +2,11 @@ import { PageContainer } from "@components/layouts";
 import { Footer, Navbar } from "@components/modules";
 import { Button, VisuallyHidden } from "@components/widgets";
 import { HomeProps } from "@core/types";
-import {
-  firstProfileBioVariants,
-  firstProfilePicVariants,
-  scrollDown,
-  secondProfileBioVariants,
-  secondProfilePicVariants,
-} from "@core/utils";
-import { motion, useAnimationControls } from "framer-motion";
+import { scrollDown } from "@core/utils";
+import { useAnimationControls } from "framer-motion";
 import Lottie from "lottie-react";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import {
-  BehanceLogo,
-  GithubLogo,
-  InstagramLogo,
-  LinkedinLogo,
-} from "phosphor-react";
 import { useEffect, useState } from "react";
 
 export const Home = ({ cmsData }: HomeProps) => {
@@ -115,7 +103,7 @@ export const Home = ({ cmsData }: HomeProps) => {
           {highlightedCases.title}
         </h2>
 
-        <div className="flex flex-col items-center gap-5">
+        <div className="mb-5 flex flex-col items-center">
           {highlightedCases.cases.map((caseItem) => (
             <Link
               key={caseItem.id}
@@ -138,136 +126,110 @@ export const Home = ({ cmsData }: HomeProps) => {
               </a>
             </Link>
           ))}
-
-          <Link href={isInEnglish ? "/en/projects" : "/projects"}>
-            <a>
-              <Button className="w-[180px] sm:w-[280px]">
-                {isInEnglish ? "See more projects" : "Ver mais projetos"}
-              </Button>
-            </a>
-          </Link>
         </div>
+
+        <Link href={isInEnglish ? "/en/projects" : "/projects"}>
+          <a>
+            <Button className="w-[180px] sm:w-[280px] mx-auto">
+              {isInEnglish ? "See more projects" : "Ver mais projetos"}
+            </Button>
+          </a>
+        </Link>
       </section>
 
       {/* People */}
       <section
         id="about"
-        className="relative w-4/5 sm:w-auto mx-auto mb-16 sm:mb-32 text-center"
+        className="w-full mb-16 sm:mb-32 flex flex-col lg:flex-row lg:justify-between items-center"
       >
-        <div className="flex flex-col xl:flex-row xl:items-center">
-          <motion.div
-            className={`xl:flex-1 xl:mr-4 ${
-              step > 0 && "lg:opacity-0"
-            } transition-all`}
-          >
-            <h2 className="mb-2 sm:mb-4 sm:text-5xl xl:text-start">
-              {about.title}
-            </h2>
+        <div className="lg:max-w-[33.33%] text-center lg:text-start">
+          <h2 className="mb-4 sm:text-5xl">{about.title}</h2>
 
-            <p className="w-2/3 xl:w-auto mx-auto mb-8 text-[8px] sm:text-xl xl:text-start">
-              {about.subtitle}
-            </p>
+          <p className="mb-4 text-[8px] sm:text-base">{about.subtitle}</p>
 
-            <Button
-              type="button"
-              onClick={() => setStep(step >= 2 ? 0 : ((step + 1) as 0 | 1 | 2))}
-              className="w-[200px] hidden xl:flex"
-            >
-              {isInEnglish ? "See more" : "Ver mais"}
-            </Button>
-          </motion.div>
-
-          {/* profile about */}
-          <div className="xl:flex-[2] mb-8 flex justify-center xl:justify-end">
-            {/* profile pictures */}
-            <motion.img
-              animate={profilePicture}
-              variants={firstProfilePicVariants}
-              src={about.people[0].profilePicture.url}
-              alt={`${about.people[0].personName} profile picture`}
-              className={`max-w-[200px] sm:max-w-[300px] md:max-w-[450px] rounded-full z-10 transition-all`}
-            />
-
-            <motion.img
-              animate={profilePicture}
-              variants={secondProfilePicVariants}
-              src={about.people[1].profilePicture.url}
-              alt={`${about.people[1].personName} profile picture`}
-              className={`max-w-[200px] sm:max-w-[300px] md:max-w-[450px] rounded-full z-0 transition-all`}
-            />
-
-            {/* profile bios */}
-            <motion.div
-              animate={profileBio}
-              variants={firstProfileBioVariants}
-              className={`absolute top-1/3 sm:top-1/2 lg:top-1/3 left-1/2 opacity-0 transition-all`}
-            >
-              <p className="text-[8px] sm:text-base">
-                {about.people[0].bio.text}
-              </p>
-
-              <ul className="mt-4 flex justify-center items-center gap-2">
-                {about.people[0].socialMedia.map((media) => (
-                  <li key={media.socialMedia}>
-                    <a href={media.url} target="_blank" rel="noreferrer">
-                      {media.socialMedia === "behance" && (
-                        <BehanceLogo size={30} />
-                      )}
-                      {media.socialMedia === "instagram" && (
-                        <InstagramLogo size={30} />
-                      )}
-                      {media.socialMedia === "github" && (
-                        <GithubLogo size={30} />
-                      )}
-                      {media.socialMedia === "linkedin" && (
-                        <LinkedinLogo size={30} />
-                      )}
-                    </a>
-                  </li>
-                ))}
-              </ul>
-            </motion.div>
-
-            <motion.div
-              animate={profileBio}
-              variants={secondProfileBioVariants}
-              className={`absolute top-1/3 sm:top-1/2 lg:top-1/3 left-1/2 opacity-0 transition-all`}
-            >
-              <p className="text-[8px] sm:text-base">
-                {about.people[1].bio.text}
-              </p>
-
-              <ul className="mt-4 flex justify-center items-center gap-2">
-                {about.people[1].socialMedia.map((media) => (
-                  <li key={media.socialMedia}>
-                    <a href={media.url} target="_blank" rel="noreferrer">
-                      {media.socialMedia === "behance" && (
-                        <BehanceLogo size={30} />
-                      )}
-                      {media.socialMedia === "instagram" && (
-                        <InstagramLogo size={30} />
-                      )}
-                      {media.socialMedia === "github" && (
-                        <GithubLogo size={30} />
-                      )}
-                      {media.socialMedia === "linkedin" && (
-                        <LinkedinLogo size={30} />
-                      )}
-                    </a>
-                  </li>
-                ))}
-              </ul>
-            </motion.div>
-          </div>
+          <Link href={isInEnglish ? "en/about" : "/about"}>
+            <a className="w-fit hidden lg:block">
+              <Button type="button" className="w-[280px]">
+                {isInEnglish ? "See more" : "Ver mais"}
+              </Button>
+            </a>
+          </Link>
         </div>
 
-        <Button
-          type="button"
-          onClick={() => setStep(step >= 2 ? 0 : ((step + 1) as 0 | 1 | 2))}
-          className={`w-[148px] mx-auto ${step > 0 ? "" : "xl:hidden"}`}
+        <svg
+          viewBox="0 0 230 231"
+          fill="none"
+          className="flex-[3] w-3/4 max-w-[280px] lg:max-w-[505px] mx-auto lg:mx-0"
         >
-          {isInEnglish ? "See more" : "Ver mais"}
-        </Button>
+          <rect
+            x="43.752"
+            y="34"
+            width="163.248"
+            height="163.248"
+            fill="#E5F2C9"
+          />
+          <g filter="url(#filter0_d_1096_33)">
+            <rect
+              x="27"
+              y="54.3169"
+              width="122.614"
+              height="122.614"
+              fill="black"
+            />
+          </g>
+          <path
+            d="M108.622 138.524V133.445C108.622 130.751 107.552 128.167 105.647 126.262C103.742 124.357 101.158 123.287 98.4635 123.287H78.1467C75.4525 123.287 72.8687 124.357 70.9636 126.262C69.0585 128.167 67.9883 130.751 67.9883 133.445V138.524M98.4636 102.97C98.4636 108.58 93.9155 113.128 88.3052 113.128C82.6948 113.128 78.1467 108.58 78.1467 102.97C78.1467 97.3596 82.6948 92.8115 88.3052 92.8115C93.9155 92.8115 98.4636 97.3596 98.4636 102.97Z"
+            stroke="#F2F2F8"
+            strokeWidth="5.07921"
+            strokeLinecap="square"
+            strokeLinejoin="round"
+          />
+          <defs>
+            <filter
+              id="filter0_d_1096_33"
+              x="0.228415"
+              y="0.773724"
+              width="229.7"
+              height="229.7"
+              filterUnits="userSpaceOnUse"
+              colorInterpolationFilters="sRGB"
+            >
+              <feFlood floodOpacity="0" result="BackgroundImageFix" />
+              <feColorMatrix
+                in="SourceAlpha"
+                type="matrix"
+                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                result="hardAlpha"
+              />
+              <feOffset dx="26.7716" />
+              <feGaussianBlur stdDeviation="26.7716" />
+              <feComposite in2="hardAlpha" operator="out" />
+              <feColorMatrix
+                type="matrix"
+                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.4 0"
+              />
+              <feBlend
+                mode="normal"
+                in2="BackgroundImageFix"
+                result="effect1_dropShadow_1096_33"
+              />
+              <feBlend
+                mode="normal"
+                in="SourceGraphic"
+                in2="effect1_dropShadow_1096_33"
+                result="shape"
+              />
+            </filter>
+          </defs>
+        </svg>
+
+        <Link href={isInEnglish ? "en/about" : "/about"}>
+          <a className="w-fit mx-auto block lg:hidden">
+            <Button type="button" className="w-[148px]">
+              {isInEnglish ? "See more" : "Ver mais"}
+            </Button>
+          </a>
+        </Link>
       </section>
 
       <Footer />
