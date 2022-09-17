d62238566558c9f035386bf226a7b825b4a07adb
Gabriel <gabrielvitor.frasao@gmail.com>
2022-09-17 15:25:42 -0300

    feat(budget): add formsubmit.co flow
    send email about new submissions with formsubmit.co

--- components/pages/Budget/Budget.tsx
+++ components/pages/Budget/Budget.tsx
@@ -2,14 +2,11 @@ import { PageContainer } from "@components/layouts";
 import { Footer, Navbar } from "@components/modules";
 import { Button, Input } from "@components/widgets";
 import { useRouter } from "next/router";
-import { useRef } from "react";
 import { PatternFormat } from "react-number-format";
 
 export const Budget = () => {
   const isInEnglish = useRouter().pathname.includes("/en");
-  const select1Ref = useRef<HTMLDivElement>(null);
-  const select2Ref = useRef<HTMLDivElement>(null);
-  const select3Ref = useRef<HTMLDivElement>(null);
+  const { submitted } = useRouter().query;
 
   return (
     <PageContainer
@@ -31,7 +28,32 @@ export const Budget = () => {
               : "Preencha o formulário abaixo para iniciar a experiência"}
           </p>
 
-          <form action="" method="POST" className="w-full flex flex-col gap-4">
+          <form
+            action="https://formsubmit.co/contato@encestudio.com"
+            method="POST"
+            className="w-full flex flex-col gap-4"
+          >
+            {/* formsubmit config */}
+            <header>
+              <input
+                type="hidden"
+                name="_next"
+                value={`https://project-ence.vercel.app${
+                  isInEnglish ? "/en" : ""
+                }/budget#submitted?submitted=true`}
+              />
+
+              <input
+                type="hidden"
+                name="_subject"
+                value="Experiência iniciada!"
+              />
+
+              <input type="hidden" name="_captcha" value="false" />
+
+              <input type="hidden" name="_template" value="box" />
+            </header>
+
             <Input
               type="text"
               name="name"
@@ -57,59 +79,116 @@ export const Budget = () => {
               defaultValue="+55 (__) _____-____"
               allowEmptyFormatting
               mask="_"
-              className="w-full px-2 py-2 bg-secondary border-2 border-black rounded-sm"
+              className="w-full px-2 py-2 bg-secondary font-semibold border-2 border-black rounded-sm"
+            />
+
+            <Input
+              type="text"
+              name="social-media"
+              placeholder={
+                isInEnglish
+                  ? "What are the social media of your company?"
+                  : "Quais são as redes da sua empresa?"
+              }
+              description={
+                isInEnglish
+                  ? "(if not applicable, specify)"
+                  : "(caso não tenha, especifique)"
+              }
+              required
+            />
+
+            <Input
+              type="text"
+              name="company-area"
+              placeholder={
+                isInEnglish
+                  ? "In which market does your company operate?"
+                  : "Em qual mercado sua empresa atua?"
+              }
+              required
             />
 
             <select
               name="company-size"
-              className="w-full px-2 py-2 bg-secondary border-2 border-black rounded-sm"
+              required
+              className="w-full px-2 py-2 bg-secondary font-semibold border-2 border-black rounded-sm"
             >
               <option value="" disabled selected>
                 {isInEnglish
                   ? "How big is your company?"
                   : "Qual é o tamanho da sua empresa?"}
               </option>
-              <option value="small">{isInEnglish ? "Small" : "Pequena"}</option>
-              <option value="medium">{isInEnglish ? "Medium" : "Média"}</option>
+
+              <option value="small">
+                {isInEnglish ? "Small" : "Profissional liberal"}
+              </option>
+
+              <option value="small">
+                {isInEnglish
+                  ? "Micro (2-10 employees)"
+                  : "Micro (2-10 funcionários)"}
+              </option>
+
+              <option value="small">
+                {isInEnglish
+                  ? "Small (11-50 employees"
+                  : "Pequena (11-50 funcionários)"}
+              </option>
+
+              <option value="medium">
+                {isInEnglish
+                  ? "Medium (50-100 employees)"
+                  : "Média (50-100 funcionários)"}
+              </option>
+
+              <option value="medium">
+                {isInEnglish
+                  ? "Big (100+ employees)"
+                  : "Grande (100+ funcionários)"}
+              </option>
             </select>
 
             <select
-              name="company-area"
-              className="w-full px-2 py-2 bg-secondary border-2 border-black rounded-sm"
+              name="experience"
+              required
+              className="w-full px-2 py-2 bg-secondary font-semibold border-2 border-black rounded-sm"
             >
               <option value="" disabled selected>
                 {isInEnglish
-                  ? "What is the company area?"
-                  : "Onde sua empresa atua?"}
+                  ? "Which experience are you looking for?"
+                  : "Qual experiência está procurando?"}
               </option>
-              <option value="small">Small</option>
-              <option value="medium">Medium</option>
-            </select>
 
-            <select
-              name="service"
-              className="w-full px-2 py-2 bg-secondary border-2 border-black rounded-sm"
-            >
-              <option value="" disabled selected>
+              <option value="visual-universe">
                 {isInEnglish
-                  ? "Which service are you looking for?"
-                  : "Qual serviço procura?"}
+                  ? "Creation of visual universe"
+                  : "Criação de universo visual"}
               </option>
-              <option value="ui-ux-design">UI/UX Design</option>
-              <option value="medium">Medium</option>
+
+              <option value="ui-ux-design">
+                {isInEnglish
+                  ? "Development of UI/UX Design for websites and landing pages"
+                  : "Desenvolvimento de UI/UX Design para sites e landing pages"}
+              </option>
             </select>
 
-            <div className="mt-8 flex flex-col sm:flex-row items-center gap-8">
+            <footer className="mt-8 flex flex-col sm:flex-row items-center gap-8">
               <Button type="submit" className="w-[240px]">
                 {isInEnglish ? "Request budget" : "Solicitar orçamento"}
               </Button>
 
-              <p className="text-[8px] sm:text-base">
+              <p
+                id="submitted"
+                className={`text-[8px] sm:text-base ${
+                  submitted ? "block" : "hidden"
+                }`}
+              >
                 {isInEnglish
                   ? "Thank you very much! This was the first step, wait for new updates."
                   : "Muito obrigado! Esse foi o primeiro passo, aguarde novas atualizações."}
               </p>
-            </div>
+            </footer>
           </form>
         </main>
 
