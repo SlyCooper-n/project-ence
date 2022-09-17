d4afe9130d5ddc42ab59cf828f958c2a352dcc0b
Gabriel <gabrielvitor.frasao@gmail.com>
2022-09-17 10:28:48 -0300

    refactor(budget): update layout
    change title, add subtitle, change inputs placeholder, add form footer

--- components/pages/Budget/Budget.tsx
+++ components/pages/Budget/Budget.tsx
@@ -12,26 +12,30 @@ export const Budget = () => {
   const select3Ref = useRef<HTMLDivElement>(null);
 
   return (
-    <PageContainer headTitle="Ence | ">
+    <PageContainer
+      headTitle={isInEnglish ? "Ence | Budget" : "Ence | Orçamento"}
+    >
       <div className="min-h-screen flex flex-col">
         <Navbar />
 
-        <main className="flex-1 flex flex-col justify-center items-center text-center">
-          <h1 className="text-xs sm:text-lg mb-8">
+        <main className="flex-1 flex flex-col justify-center">
+          <h1 className="mb-4 text-lg sm:text-2xl">
             {isInEnglish
-              ? "Fill in the form below and we will contact you as soon as possible."
-              : "Preencha o formulário abaixo e entraremos em contato com você o mais rápido possível."}
+              ? "Beginning of the experience-"
+              : "Início da experiência-"}
           </h1>
 
-          <form
-            action=""
-            method="POST"
-            className="w-full flex flex-col items-center gap-4"
-          >
+          <p className="mb-12 text-[8px] sm:text-base">
+            {isInEnglish
+              ? "Fill in the form below to start the experience"
+              : "Preencha o formulário abaixo para iniciar a experiência"}
+          </p>
+
+          <form action="" method="POST" className="w-full flex flex-col gap-4">
             <Input
               type="text"
               name="name"
-              placeholder={isInEnglish ? "Your name" : "Seu nome"}
+              placeholder={isInEnglish ? "Your name-" : "Seu nome-"}
               autoFocus
               required
             />
@@ -39,11 +43,13 @@ export const Budget = () => {
             <Input
               type="text"
               name="company"
-              placeholder={isInEnglish ? "Company name" : "Nome da sua empresa"}
+              placeholder={
+                isInEnglish ? "Company name-" : "Nome da sua empresa-"
+              }
               required
             />
 
-            <Input type="email" name="email" placeholder="Email" required />
+            <Input type="email" name="email" placeholder="Email-" required />
 
             <PatternFormat
               name="phone"
@@ -51,34 +57,12 @@ export const Budget = () => {
               defaultValue="+55 (__) _____-____"
               allowEmptyFormatting
               mask="_"
-              className="w-full px-2 py-1 border border-primary rounded-sm"
+              className="w-full px-2 py-2 bg-secondary border-2 border-black rounded-sm"
             />
-
-            {/* <Select
-              container={select1Ref.current!}
-              placeholder={
-                isInEnglish
-                  ? "How big is your company?"
-                  : "Qual é o tamanho da sua empresa?"
-              }
-            >
-              <SelectItem
-                value="small"
-                text={isInEnglish ? "Small" : "Pequena"}
-              />
-              <SelectItem
-                value="medium"
-                text={isInEnglish ? "Medium" : "Média"}
-              />
-            </Select>
-            <div
-              ref={select1Ref}
-              className="w-full -mt-2 flex justify-center"
-            /> */}
 
             <select
               name="company-size"
-              className="w-full px-2 py-1 border border-primary rounded-sm"
+              className="w-full px-2 py-2 bg-secondary border-2 border-black rounded-sm"
             >
               <option value="" disabled selected>
                 {isInEnglish
@@ -91,7 +75,7 @@ export const Budget = () => {
 
             <select
               name="company-area"
-              className="w-full px-2 py-1 border border-primary rounded-sm"
+              className="w-full px-2 py-2 bg-secondary border-2 border-black rounded-sm"
             >
               <option value="" disabled selected>
                 {isInEnglish
@@ -104,7 +88,7 @@ export const Budget = () => {
 
             <select
               name="service"
-              className="w-full px-2 py-1 border border-primary rounded-sm"
+              className="w-full px-2 py-2 bg-secondary border-2 border-black rounded-sm"
             >
               <option value="" disabled selected>
                 {isInEnglish
@@ -115,10 +99,17 @@ export const Budget = () => {
               <option value="medium">Medium</option>
             </select>
 
-            {/* company size, company occupation, service */}
-            <Button type="submit" className="w-[148px]">
-              {isInEnglish ? "Send" : "Enviar"}
-            </Button>
+            <div className="mt-8 flex items-center gap-8">
+              <Button type="submit" className="w-[240px]">
+                {isInEnglish ? "Request budget" : "Solicitar orçamento"}
+              </Button>
+
+              <p>
+                {isInEnglish
+                  ? "Thank you very much! This was the first step, wait for new updates."
+                  : "Muito obrigado! Esse foi o primeiro passo, aguarde novas atualizações."}
+              </p>
+            </div>
           </form>
         </main>
 
