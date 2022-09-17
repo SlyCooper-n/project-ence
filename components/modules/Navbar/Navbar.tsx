65fb4d1cbaa7c039dc23a9c9e56dfca9c4095df6
Gabriel <gabrielvitor.frasao@gmail.com>
2022-09-17 13:51:47 -0300

    feat(navbar): add menu list
    update menu options, add menu animation

--- components/modules/Navbar/Navbar.tsx
+++ components/modules/Navbar/Navbar.tsx
@@ -1,31 +1,42 @@
 import { NavbarProps } from "@core/types";
+import { menuItems, menuList, menuOptions } from "@core/utils";
+import { motion } from "framer-motion";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-export const Navbar = ({ translucent, absolute, className }: NavbarProps) => {
-  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+export const Navbar = ({
+  translucent,
+  absolute,
+  onMenuOpen,
+  className,
+}: NavbarProps) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const router = useRouter();
 
   const isInEnglish = router.pathname.includes("/en");
 
+  useEffect(() => {
+    onMenuOpen && onMenuOpen(isMenuOpen);
+  }, [isMenuOpen, onMenuOpen]);
+
   function handleToggleMenu() {
-    setIsMobileMenuOpen((prev) => !prev);
+    setIsMenuOpen((prev) => !prev);
   }
 
   return (
     <nav
-      className={`py-5 sm:py-8 flex justify-between items-center text-black z-50 ${
+      className={`py-5 sm:py-8 flex flex-row-reverse sm:flex-row justify-between items-center text-black z-50 ${
         translucent ? "bg-opacity-70 bg-base-100" : ""
       } ${
         absolute ? "sm:absolute sm:top-0 sm:left-0 sm:w-full sm:px-[5vw]" : ""
       } ${className}`}
     >
-      {/* menu bar on mobile screens */}
+      {/* menu bar */}
       <button
         type="button"
         onClick={handleToggleMenu}
-        className="relative w-16 mr-auto z-50"
+        className="relative w-16 ml-auto sm:mr-auto sm:ml-0 z-50"
       >
         <div
           className={`absolute top-1/2 -translate-y-1/2 w-full h-3 bg-black`}
@@ -34,17 +45,17 @@ export const Navbar = ({ translucent, absolute, className }: NavbarProps) => {
 
       {!router.pathname.includes("/budget") ? (
         <Link href={isInEnglish ? "/en/budget" : "/budget"}>
-          <a className="relative hidden sm:block">
+          <a className="relative hidden sm:block z-50">
             {isInEnglish ? "Start experience" : "Iniciar experiência"}
           </a>
         </Link>
       ) : (
-        <span className="hidden sm:block">
+        <span className="hidden sm:block z-50">
           {isInEnglish ? "Experience started" : "Experiência iniciada"}
         </span>
       )}
 
-      <div className="divider hidden sm:block w-[13vw] h-1 mx-4 bg-black" />
+      <div className="divider hidden sm:block w-[13vw] h-1 mx-4 bg-black z-50" />
 
       {/* language toggler buttons */}
       <div className="flex">
@@ -59,7 +70,7 @@ export const Navbar = ({ translucent, absolute, className }: NavbarProps) => {
           }}
         >
           <a
-            className={`relative flex mr-3 z-50 ${
+            className={`relative flex mr-3 font-semibold z-50 ${
               isInEnglish ? "" : "lang-active"
             }`}
           >
@@ -78,7 +89,7 @@ export const Navbar = ({ translucent, absolute, className }: NavbarProps) => {
           }}
         >
           <a
-            className={`relative flex mr-3 z-50 ${
+            className={`relative flex mr-3 font-semibold z-50 ${
               isInEnglish ? "lang-active" : ""
             }`}
           >
@@ -86,6 +97,73 @@ export const Navbar = ({ translucent, absolute, className }: NavbarProps) => {
           </a>
         </Link>
       </div>
+
+      {/* menu */}
+      <motion.ul
+        variants={menuList}
+        animate={isMenuOpen ? "show" : "hidden"}
+        className="fixed top-0 -right-full w-screen h-screen pl-16 pt-16 flex flex-col justify-center gap-4 sm:gap-8 bg-secondary z-[49]"
+      >
+        {menuOptions.map((opt) => {
+          if (
+            router.pathname.includes("/budget") &&
+            opt.name.includes("Iniciar experiência")
+          ) {
+            return (
+              <motion.span
+                variants={menuItems}
+                animate={isMenuOpen ? "show" : "hidden"}
+                className="sm:text-5xl font-semibold"
+              >
+                {isInEnglish ? "Experience started" : "Experiência iniciada"}
+              </motion.span>
+            );
+          }
+
+          return (
+            <motion.li
+              key={opt.name}
+              variants={menuItems}
+              animate={isMenuOpen ? "show" : "hidden"}
+            >
+              <Link href={isInEnglish ? opt.en_path : opt.path}>
+                <motion.a className="relative font-semibold sm:text-5xl cursor-pointer">
+                  {isInEnglish ? opt.en_name : opt.name}
+                </motion.a>
+              </Link>
+            </motion.li>
+          );
+        })}
+
+        <motion.li
+          variants={menuItems}
+          animate={isMenuOpen ? "show" : "hidden"}
+        >
+          <ul className="mt-12 flex gap-4 text-[8px] sm:text-base">
+            <li>
+              <a
+                href="https://www.behance.net/joaovitorn-ence/moodboards"
+                rel="noreferrer"
+                target="_blank"
+                className="relative"
+              >
+                Behance
+              </a>
+            </li>
+
+            <li>
+              <a
+                href="www.instagram.com/encestudio"
+                rel="noreferrer"
+                target="_blank"
+                className="relative"
+              >
+                Instagram
+              </a>
+            </li>
+          </ul>
+        </motion.li>
+      </motion.ul>
     </nav>
   );
 };
