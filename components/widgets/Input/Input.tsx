c1d93c3d958e95ce4e9f1b117c86dc1677dac0bc
Gabriel <gabrielvitor.frasao@gmail.com>
2022-09-17 10:27:13 -0300

    refactor(input): update layout

--- components/widgets/Input/Input.tsx
+++ components/widgets/Input/Input.tsx
@@ -17,14 +17,14 @@ export const Input = ({
         value={inputValue}
         onChange={(e) => setInputValue(e.target.value)}
         className={`${
-          inputRef.current?.value.length! > 0 ? "pt-4 pb-2" : "py-1"
-        } w-full px-2 focus:pt-4 focus:pb-2 border border-primary rounded-sm transition-all duration-500 placeholder-shown:placeholder:opacity-0`}
+          inputRef.current?.value.length! > 0 ? "pt-4 pb-2" : "py-2"
+        } w-full px-2 focus:pt-4 focus:pb-2 bg-secondary border-2 border-black rounded-sm transition-all duration-500 placeholder-shown:placeholder:opacity-0`}
       />
 
       <span
         className={`${
           inputRef.current?.value.length! > 0 ? "top-2 text-xs" : "top-1/2"
-        } absolute left-2 -translate-y-1/2 group-focus-within:top-2 group-focus-within:-translate-x-0 group-focus-within:text-xs pointer-events-none`}
+        } absolute left-2 -translate-y-1/2 font-semibold group-focus-within:top-2 group-focus-within:-translate-x-0 group-focus-within:text-xs pointer-events-none transition-all duration-500`}
       >
         {placeholder}
       </span>
