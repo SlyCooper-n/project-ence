cdb81a0507c19c666f083dec09ab5f20a28c2b02
Gabriel VS Frasão <gabrielvitor.frasao@gmail.com>
2022-10-21 08:27:09 -0300

    Update index.ts

--- core/services/graphql/index.ts
+++ core/services/graphql/index.ts
@@ -2,12 +2,6 @@ import { GraphQLClient } from "graphql-request";
 
 export const hygraph = new GraphQLClient(
   "https://api-sa-east-1.hygraph.com/v2/cl6l5c9q60f0501tb57ll9xsz/master"
-  // {
-  //   headers: {
-  //     Authorization:
-  //       "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImdjbXMtbWFpbi1wcm9kdWN0aW9uIn0.eyJ2ZXJzaW9uIjozLCJpYXQiOjE2NjAxNjk0MjcsImF1ZCI6WyJodHRwczovL2FwaS1zYS1lYXN0LTEuaHlncmFwaC5jb20vdjIvY2w2bDVjOXE2MGYwNTAxdGI1N2xsOXhzei9tYXN0ZXIiLCJtYW5hZ2VtZW50LW5leHQuZ3JhcGhjbXMuY29tIl0sImlzcyI6Imh0dHBzOi8vbWFuYWdlbWVudC5ncmFwaGNtcy5jb20vIiwic3ViIjoiN2ZhYzM3MTEtMGU1YS00YjFkLTg5MjctYjVjZDAzNDgzODEyIiwianRpIjoiY2w2bzYwZ3J5MHA0YjAxdXBlcXUyMXN6MiJ9.18z5EW_-i5t0K30r6ISpiNV7bNYWNnvovdGnXivD6D7CRGSbcop57rU5zJ_ZM-ORhvABSOPWFQmABsysqUieh6AkwlTYuO0a1Lq47TxAYp8FlwXupz-CqT_iw-kuZeOMTLvb55MHg6XN6n0ZtqIshyqaJg9_Khdq2U3EdnlmbkQH7bcvBW6ijyh0tvImd1SoULpM93DDn5HpFHETKE9qK003JinVvCcz0bTfSJdhbNJcx12mReyCMWWwkKAkImP_6EBoM1-0Mj7LhCe0wNjZVrwQa6gCQndFreDH0M2IV65_-Ao__nKcA5nWcqqywDuIjXspZ3j_dETvU4cX7i19GoMbkTqCI6jsiTcTkpV0Alu8YUleprWCb1xKFywQAs5A6hhr-t4nW7TTu6a9lkpKrzdVRXSwfROpoFKDbz0YNcs6Z-UyG3-cO--QjbVqwY1zAGlInYC1ymwwCk5DOkGPJ63DiPGpJPs8yGkOLFzVnJzNB43_C38vHybda_AvNrdhO78ai20cTy-_ATkter9HXa34QW6Y-le2A5wimOGQfpNZZTHS_uFAiyWdv4GKolXRmAaVw-aK-Eg03WqJ_5kQabMGuGZeuTOSqYZrcq4cB5BL7S8MlbgMeFsbHNbzSRw57w9RuyjWzv3RylyXrUFK4gPePzKwlJHrDOviZIwn7Zc",
-  //   },
-  // }
 );
 
 export * from "./queries";
