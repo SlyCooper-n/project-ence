f495f65922aa5748572186ef2756f69d2e0586b0
Gabriel <gabrielvitor.frasao@gmail.com>
2023-03-08 16:51:48 -0300

    refactor: create highlighted cases content category

--- src/components/layouts/CasesSection/CasesSection.tsx
+++ src/components/layouts/CasesSection/CasesSection.tsx
@@ -1,12 +1,23 @@
+import Link from "next/link";
+
 import { Button, Container, Heading } from "@/components";
 import content from "@/content.json";
 import { useLang } from "@/hooks";
-import Link from "next/link";
 import { Case } from "./components";
 
 export const CasesSection = () => {
   const { isInEnglish } = useLang();
-  const { cases } = content;
+
+  const { cases, highlightedCasesId } = content;
+  const firstCase = cases.find((item) => item.id === highlightedCasesId[0]);
+  const secondCase = cases.find((item) => item.id === highlightedCasesId[1]);
+  const thirdCase = cases.find((item) => item.id === highlightedCasesId[2]);
+
+  if (!firstCase || !secondCase || !thirdCase) {
+    throw new Error(
+      "Could not find highlighted cases. Please review content.json",
+    );
+  }
 
   return (
     <Container asChild className="py-16">
@@ -15,28 +26,28 @@ export const CasesSection = () => {
 
         <div className="mb-12 flex flex-col gap-9 sm:gap-3">
           <Case
-            slug={cases[0].slug}
-            name={cases[0].name}
-            slogan={cases[0].slogan}
-            date={cases[0].date}
-            bannerUrl={cases[0].bannerUrl}
+            slug={firstCase.slug}
+            name={firstCase.name}
+            slogan={firstCase.slogan}
+            date={firstCase.date}
+            bannerUrl={firstCase.highlightedBannerUrl}
           />
 
           <div className="flex flex-col gap-9 sm:flex-row sm:gap-3">
             <Case
-              slug={cases[1].slug}
-              name={cases[1].name}
-              slogan={cases[1].slogan}
-              date={cases[1].date}
-              bannerUrl={cases[1].bannerUrl}
+              slug={secondCase.slug}
+              name={secondCase.name}
+              slogan={secondCase.slogan}
+              date={secondCase.date}
+              bannerUrl={secondCase.highlightedBannerUrl}
             />
 
             <Case
-              slug={cases[2].slug}
-              name={cases[2].name}
-              slogan={cases[2].slogan}
-              date={cases[2].date}
-              bannerUrl={cases[2].bannerUrl}
+              slug={thirdCase.slug}
+              name={thirdCase.name}
+              slogan={thirdCase.slogan}
+              date={thirdCase.date}
+              bannerUrl={thirdCase.highlightedBannerUrl}
             />
           </div>
         </div>
