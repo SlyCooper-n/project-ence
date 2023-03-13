710c380de16a340bcd30b6dddfd80669e6a848c7
Gabriel <gabrielvitor.frasao@gmail.com>
2023-03-12 23:33:00 -0300

    refactor: update page layout and data structure

--- src/components/pages/CaseDetails.tsx
+++ src/components/pages/CaseDetails.tsx
@@ -13,118 +13,109 @@ export const CaseDetails = ({ caseId }: CaseDetailsProps) => {
   const caseItem = content.cases.find((item) => item.id === caseId);
   if (!caseItem) throw new Error(`Error trying to find case of id ${caseId}`);
   const {
-    order,
     name,
     slogan,
     description,
-    dataSheet,
     date,
+    services,
+    frontEnd,
     designStrategy,
     images,
     nextCaseSlug,
   } = caseItem;
 
   return (
-    <Container className="py-16">
-      <main className="mb-24 md:mb-64">
-        <Text className="mb-12 block md:text-xl">
-          {order}/{content.cases.length} Cases
-        </Text>
+    <>
+      <img
+        src=""
+        alt={`${name} banner`}
+        className="mx-auto h-[758px] w-full max-w-[1920px]"
+      />
 
-        <Heading
-          asChild
-          className="mb-12 text-2xl md:mb-24 md:text-4xl lg:text-6xl"
-        >
-          <h1>
-            {name} - {slogan[activeLanguage]}.
-          </h1>
-        </Heading>
-
-        <div className="mb-6 flex flex-col gap-6 md:mb-9 md:flex-row md:justify-between">
-          <Text asChild className="max-w-lg md:text-xl">
-            <p>{description[activeLanguage]}</p>
+      <Container className="py-16">
+        <main className="mb-24 md:mb-64">
+          <Text className="mb-2 block font-semibold">
+            {isInEnglish ? "About:" : "Sobre:"}
           </Text>
 
-          <div className="min-w-[256px] md:text-xl">
-            <Heading className="mb-4">
-              {isInEnglish ? "Data sheet -" : "Ficha técnica -"}
+          <Heading asChild className="mb-7 text-2xl md:mb-20">
+            <h1>{name}</h1>
+          </Heading>
+
+          <ul className="mb-16 flex flex-col gap-4 md:mb-28 md:flex-row md:gap-16 md:text-xl">
+            <li>
+              <strong>{isInEnglish ? "Date" : "Data"}</strong>: {date}
+            </li>
+
+            <li>
+              <strong>{isInEnglish ? "Services" : "Serviços"}</strong>:{" "}
+              {services[activeLanguage].join(", ")}
+            </li>
+
+            {frontEnd ? (
+              <li>
+                <strong>Front-End</strong>: {frontEnd}
+              </li>
+            ) : null}
+          </ul>
+
+          <div className="flex flex-col gap-12 md:flex-row md:items-center md:justify-between">
+            <Heading className="text-2xl md:text-6xl">
+              {slogan[activeLanguage]}
             </Heading>
 
-            <ul>
-              <li>
-                <strong>{isInEnglish ? "Services" : "Serviços"}:</strong>{" "}
-                {dataSheet.services.join(", ")}
-              </li>
+            <div className="flex flex-col gap-7">
+              <Text asChild>
+                <p>{description[activeLanguage]}</p>
+              </Text>
 
-              <li>
-                <strong>
-                  {isInEnglish ? "Art director" : "Diretor de arte"}:
-                </strong>{" "}
-                {dataSheet.artDirector}
-              </li>
+              <div>
+                <Heading className="mb-4">
+                  {isInEnglish ? "Design strategy" : "Estratégia de design"}
+                </Heading>
 
-              <li>
-                <strong>Design:</strong> {dataSheet.design}
-              </li>
-
-              {dataSheet.dev ? (
-                <li>
-                  <strong>
-                    {isInEnglish ? "Developer" : "Desenvolvedor"}:
-                  </strong>{" "}
-                  {dataSheet.dev}
-                </li>
-              ) : null}
-
-              <li>
-                <strong>{isInEnglish ? "Date" : "Data"}:</strong> {date}
-              </li>
-            </ul>
+                <Text asChild>
+                  <p>{designStrategy[activeLanguage]}</p>
+                </Text>
+              </div>
+            </div>
           </div>
-        </div>
+        </main>
 
-        <Heading className="mb-4 md:text-xl">
-          {isInEnglish ? "Design strategy -" : "Estratégia de design -"}
-        </Heading>
+        <section className="mb-16 flex flex-col gap-12">
+          {images.map((img) => (
+            <img key={img} src={img} alt="" />
+          ))}
+        </section>
 
-        <Text asChild className="max-w-lg md:text-xl">
-          <p>{designStrategy[activeLanguage]}</p>
-        </Text>
-      </main>
+        <nav className="flex flex-col gap-12 md:flex-row md:justify-between">
+          <Text asChild className="max-w-sm">
+            <p>
+              <Anchor asChild className="mb-4 block w-fit">
+                <Link href={isInEnglish ? "/en/budget" : "/budget"}>
+                  {isInEnglish ? "Start experience" : "Iniciar experiência"}
+                </Link>
+              </Anchor>
 
-      <section className="mb-16 flex flex-col gap-12">
-        {images.map((img) => (
-          <img key={img} src={img} alt="" />
-        ))}
-      </section>
+              {isInEnglish
+                ? "You are just a few clicks away from developing the visual universe of your project. Get in touch below."
+                : "Você está a alguns cliques de desenvolver o universo visual do seu projeto. Entre em contato abaixo."}
+            </p>
+          </Text>
 
-      <nav className="flex flex-col gap-12 md:flex-row md:justify-between">
-        <Text asChild className="max-w-sm">
-          <p>
-            <Anchor asChild className="mb-4 block w-fit">
-              <Link href={isInEnglish ? "/en/budget" : "/budget"}>
-                {isInEnglish ? "Start experience" : "Iniciar experiência"}
-              </Link>
-            </Anchor>
-
-            {isInEnglish
-              ? "You are just a few clicks away from developing the visual universe of your project. Get in touch below."
-              : "Você está a alguns cliques de desenvolver o universo visual do seu projeto. Entre em contato abaixo."}
-          </p>
-        </Text>
-
-        <Anchor asChild className="block h-fit w-fit">
-          <Link
-            href={
-              isInEnglish
-                ? `/en/cases/${nextCaseSlug}`
-                : `/cases/${nextCaseSlug}`
-            }
-          >
-            {isInEnglish ? "Next project" : "Próximo projeto"} &gt;
-          </Link>
-        </Anchor>
-      </nav>
-    </Container>
+          <Anchor asChild className="block h-fit w-fit">
+            <Link
+              href={
+                isInEnglish
+                  ? `/en/cases/${nextCaseSlug}`
+                  : `/cases/${nextCaseSlug}`
+              }
+            >
+              {isInEnglish ? "Next project" : "Próximo projeto"} &gt;
+            </Link>
+          </Anchor>
+        </nav>
+      </Container>
+    </>
   );
 };
